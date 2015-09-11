fbc176eb6e09a7c43a13ff53aade1fd77cfadcb5
BrunoLagoa <miroldols@gmail.com>
2015-09-11 02:08:51 -0300

    - Criando route put em Router.php para Client, ClientController@update  - PROJETO FASE 1 - CLIENTS

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,3 +19,4 @@ Route::get('client', 'ClientController@index');
 Route::post('client', 'ClientController@store');
 Route::get('client/{id}', 'ClientController@show');
 Route::delete('client/{id}', 'ClientController@destroy');
+Route::put('client/{id}', 'ClientController@update');
