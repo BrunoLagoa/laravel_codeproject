fd5beb0a8515bf8d6ff634be3969473e5d85c072
BrunoLagoa <miroldols@gmail.com>
2015-09-10 23:35:42 -0300

    - Criando MassAssignment no Model Cliente  - Criando primeiro registro pelo tinker  - TRABALHANDO COM MODELS - Criando primeiro registro

--- app/Client.php
+++ app/Client.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'responsible',
+        'email',
+        'phone',
+        'address',
+        'obs'
+    ];
 }
