a7211182c329328a51bfe9b803dc6a86a54e8ef3
BrunoLagoa <miroldols@gmail.com>
2015-09-11 00:51:25 -0300

    - Criando Route client  - Criando action index do ClientController.php  - TRABALHANDO COM MODELS - Utilizando controller

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('client', function() {
-    return \CodeProject\Client::all();
-});
+Route::get('client', 'ClientController@index');
