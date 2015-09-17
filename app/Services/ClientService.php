723592f85efae7700c920e7e5ece3b5500835a9f
BrunoLagoa <miroldols@gmail.com>
2015-09-17 00:04:31 -0300

    - SERVICE LAYER - Criando validator

--- app/Services/ClientService.php
+++ app/Services/ClientService.php
@@ -4,27 +4,56 @@ namespace CodeProject\Services;
 
 
 use CodeProject\Repositories\ClientRepository;
+use CodeProject\Validators\ClientValidator;
+use Illuminate\Contracts\Validation\ValidationException;
 
 class ClientService
 {
+    /**
+     * @var ClientRepository
+     */
     protected $repository;
 
-    public function __construct(ClientRepository $repository)
+    /**
+     * @var ClientValidator
+     */
+    private $validator;
+
+    public function __construct(ClientRepository $repository, ClientValidator $validator)
     {
         $this->repository = $repository;
+        $this->validator = $validator;
     }
 
     public function create(array $data)
     {
+        try {
+            $this->validator->with($data)->passesOrFail();
+            return $this->repository->create($data);
+        }catch (ValidationException $e) {
+            return [
+                'error' => true,
+                'message' => $e->getMessageBag()
+            ];
+        }
+
         // Enviar um email
         // Disparar notificação
         // Disparar um tweet
-        return $this->repository->create($data);
+
     }
 
     public function update(array $data, $id)
     {
-        return $this->repository->update($data, $id);
+        try {
+            $this->validator->with($data)->passesOrFail();
+            return $this->repository->update($data, $id);
+        }catch (ValidationException $e) {
+            return [
+                'error' => true,
+                'message' => $e->getMessageBag()
+            ];
+        }
     }
 
 }
---
+++ app/Validators/ClientValidator.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace CodeProject\Validators;
+
+
+use Prettus\Validator\LaravelValidator;
+
+class ClientValidator extends LaravelValidator
+{
+
+    protected $rules =[
+        'name' => 'required|max:255',
+        'responsible' => 'required|max:255',
+        'email' => 'required',
+        'phone' => 'required',
+        'address' => 'required',
+    ];
+
+
+}
