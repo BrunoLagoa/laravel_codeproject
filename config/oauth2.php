30f7fd528589de5cc4c4dc9566ade80ecfec705e
BrunoLagoa <miroldols@gmail.com>
2015-10-16 18:16:49 -0300

    - LaravelAngular-6.2-Configurando OAuth 2  Changes to be committed: 	modified:   .idea/workspace.xml 	new file:   app/OAuth/Verifier.php 	modified:   config/oauth2.php

--- config/oauth2.php
+++ config/oauth2.php
@@ -28,7 +28,11 @@ return [
     */
 
     'grant_types' => [
-
+        'password' => [
+            'class' => '\League\OAuth2\Server\Grant\PasswordGrant',
+            'callback' => '\CodeProject\OAuth\Verifier@verify',
+            'access_token_ttl' => 3600
+        ],
     ],
 
     /*
