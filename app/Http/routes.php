b02d1103e894179b70481746290a425d1f405b9b
BrunoLagoa <miroldols@gmail.com>
2015-10-17 02:03:34 -0300

    - LaravelAngular-6.6-Agrupando rotas  Changes to be committed: 	modified:   app/Http/routes.php 	modified:   config/oauth2.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,29 +19,27 @@ Route::post('oauth/access_token', function() {
     return Response::json(Authorizer::issueAccessToken());
 });
 
-Route::get('client', ['middleware' => 'oauth', 'uses' => 'ClientController@index']);
-Route::post('client', 'ClientController@store');
-Route::get('client/{id}', 'ClientController@show');
-Route::put('client/{id}', 'ClientController@update');
-Route::delete('client/{id}', 'ClientController@destroy');
+Route::group(['middleware'=>'oauth'], function() {
 
-Route::get('project/{id}/note', 'ProjectNoteController@index');
-Route::post('project/{id}/note', 'ProjectNoteController@store');
-Route::get('project/{id}/note/{nodeId}', 'ProjectNoteController@show');
-Route::put('project/{id}/note/{nodeId}', 'ProjectNoteController@update');
-Route::delete('project/{id}/note/{nodeId}', 'ProjectNoteController@destroy');
+    Route::resource('client', 'ClientController', ['except' => ['create', 'edit']]);
 
-Route::get('project/{id}/task', 'ProjectTaskController@index');
-Route::post('project/{id}/task', 'ProjectTaskController@store');
-Route::get('project/{id}/task/{taskId}', 'ProjectTaskController@show');
-Route::put('project/{id}/task/{taskId}', 'ProjectTaskController@update');
-Route::delete('project/{id}/task/{taskId}', 'ProjectTaskController@destroy');
+    Route::group(['prefix' => 'project'], function() {
+        Route::resource('', 'ProjectController', ['except' => ['create', 'edit']]);
 
-Route::get('project/{id}/members', 'ProjectController@showMembers');
-Route::delete('project/{id}/member/{memberId}', ['uses' => 'ProjectController@removeMember']);
+        Route::get('/{id}/note', 'ProjectNoteController@index');
+        Route::post('/{id}/note', 'ProjectNoteController@store');
+        Route::get('/{id}/note/{nodeId}', 'ProjectNoteController@show');
+        Route::put('/{id}/note/{nodeId}', 'ProjectNoteController@update');
+        Route::delete('/{id}/note/{nodeId}', 'ProjectNoteController@destroy');
 
-Route::get('project', 'ProjectController@index');
-Route::post('project', 'ProjectController@store');
-Route::get('project/{id}', 'ProjectController@show');
-Route::put('project/{id}', 'ProjectController@update');
-Route::delete('project/{id}', 'ProjectController@destroy');
+        Route::get('/{id}/task', 'ProjectTaskController@index');
+        Route::post('/{id}/task', 'ProjectTaskController@store');
+        Route::get('/{id}/task/{taskId}', 'ProjectTaskController@show');
+        Route::put('/{id}/task/{taskId}', 'ProjectTaskController@update');
+        Route::delete('/{id}/task/{taskId}', 'ProjectTaskController@destroy');
+
+        Route::get('/{id}/members', 'ProjectController@showMembers');
+        Route::delete('/{id}/member/{memberId}', ['uses' => 'ProjectController@removeMember']);
+    });
+
+});
--- config/oauth2.php
+++ config/oauth2.php
@@ -33,6 +33,12 @@ return [
             'callback' => '\CodeProject\OAuth\Verifier@verify',
             'access_token_ttl' => 3600
         ],
+
+        'refresh_token' => [
+            'class' => '\League\OAuth2\Server\Grant\RefreshTokenGrant',
+            'access_token_ttl' => 3600,
+            'refresh_token_ttl' => 36000
+        ],
     ],
 
     /*
