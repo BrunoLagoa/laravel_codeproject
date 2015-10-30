bceb12402befec41d52f45a5273e9341ca0b9473
BrunoLagoa <miroldols@gmail.com>
2015-10-30 17:33:18 -0200

    - LaravelAngular-2.16-Tarefa gulp default  Changes to be committed: 	modified:   gulpfile.js 	new file:   public/build/css/all-0bb6111c01.css 	new file:   public/build/css/all.css.map 	new file:   public/build/js/all-9f4247363d.js 	new file:   public/build/js/all.js.map 	new file:   public/build/rev-manifest.json 	new file:   public/css/all.css 	new file:   public/css/all.css.map 	new file:   public/js/all.js 	new file:   public/js/all.js.map

--- gulpfile.js
+++ gulpfile.js
@@ -59,23 +59,18 @@ gulp.task('clear-build-folder', function(){
     clean.sync(config.build_path);
 });
 
+gulp.task('default',['clear-build-folder'], function(){
+    elixir(function(mix){
+        mix.styles(config.vendor_path_css.concat([config.assets_path + '/css/**/*.css']),
+        'public/css/all.css', config.assets_path);
+        mix.scripts(config.vendor_path_js.concat([config.assets_path + '/js/**/*.js']),
+            'public/js/all.js', config.assets_path);
+        mix.version(['js/all.js', 'css/all.css']);
+    });
+});
+
 gulp.task('watch-dev',['clear-build-folder'], function(){
     liveReload.listen();
     gulp.start('copy-styles', 'copy-scripts');
     gulp.watch(config.assets_path + '/**', ['copy-styles', 'copy-scripts']);
-});
-
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
-
-elixir(function(mix) {
-    mix.sass('app.scss');
 });
