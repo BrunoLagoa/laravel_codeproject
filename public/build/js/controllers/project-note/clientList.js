af31448ba8509cd986fd9e76bbcdfc0203ad416b
BrunoLagoa <miroldols@gmail.com>
2015-11-19 11:02:11 -0200

    - LaravelAngular-5.7-Finalizando configuracao de projectNoteShow  Changes to be committed: 	deleted:    public/build/js/controllers/project-note/clientEdit.js 	deleted:    public/build/js/controllers/project-note/clientList.js 	deleted:    public/build/js/controllers/project-note/clientNew.js 	deleted:    public/build/js/controllers/project-note/clientRemove.js 	new file:   public/build/js/controllers/project-note/projectNoteShow.js 	new file:   resources/assets/js/controllers/project-note/projectNoteShow.js 	modified:   resources/views/app.blade.php

--- public/build/js/controllers/project-note/clientList.js
+++
@@ -1,4 +0,0 @@
-angular.module('app.controllers')
-    .controller('ClientListController',['$scope','Client',function($scope,Client) {
-        $scope.clients = Client.query();
-    }]);
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -92,6 +92,7 @@
 		<script src="{{ asset('build/js/controllers/client/clientEdit.js') }}" ></script>
 		<script src="{{ asset('build/js/controllers/client/clientRemove.js') }}" ></script>
 
+		<script src="{{ asset('build/js/controllers/project-note/projectNoteShow.js') }}" ></script>
 		<script src="{{ asset('build/js/controllers/project-note/projectNoteList.js') }}" ></script>
 		<script src="{{ asset('build/js/controllers/project-note/projectNoteNew.js') }}" ></script>
 		<script src="{{ asset('build/js/controllers/project-note/projectNoteEdit.js') }}" ></script>
