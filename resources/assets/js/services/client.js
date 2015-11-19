c4a5748007c666a67609afed73cae4c99b656001
BrunoLagoa <miroldols@gmail.com>
2015-11-19 02:45:38 -0200

    - LaravelAngular-5.4-Criando transformResponse no resource Client  Changes to be committed: 	modified:   public/build/js/services/client.js 	modified:   resources/assets/js/services/client.js

--- resources/assets/js/services/client.js
+++ resources/assets/js/services/client.js
@@ -3,6 +3,15 @@ angular.module('app.services')
         return $resource(appConfig.baseUrl + '/client/:id',{id: '@id'},{
             update: {
                 method: 'PUT'
+            },
+            query: {
+                method: 'GET',
+                isArray: true,
+                transformResponse: function(data,headers){
+                    var dataJson = JSON.parse(data);
+                    dataJson = dataJson.data;
+                    return dataJson;
+                }
             }
         });
     }]);
