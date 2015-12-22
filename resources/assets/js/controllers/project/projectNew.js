1543af64519d7f378f271cea7330315734d36d3f
BrunoLagoa <miroldols@gmail.com>
2015-12-22 17:11:49 -0200

    - LaravelAngular-6.4-Iniciando com cadastro de project Changes to be committed: 	modified:   .idea/workspace.xml 	modified:   public/build/js/controllers/project/projectNew.js 	modified:   public/build/views/project/list.html 	modified:   public/build/views/project/new.html 	modified:   resources/assets/js/controllers/project/projectNew.js 	modified:   resources/assets/js/views/project/list.html 	modified:   resources/assets/js/views/project/new.html

--- resources/assets/js/controllers/project/projectNew.js
+++ resources/assets/js/controllers/project/projectNew.js
@@ -1,15 +1,13 @@
 angular.module('app.controllers')
-    .controller('ProjectNoteNewController',
-        ['$scope', '$location', '$routeParams', 'ProjectNote',
-            function ($scope, $location, $routeParams, ProjectNote) {
-                $scope.projectNote = new ProjectNote();
-                $scope.projectNote.project_id = $routeParams.id;
+    .controller('ProjectNewController',
+        ['$scope', '$location', 'Project', function ($scope, $location, Project) {
+            $scope.project = new Project();
 
-                $scope.save = function () {
-                    if ($scope.form.$valid) {
-                        $scope.projectNote.$save({id: $routeParams.id}).then(function () {
-                            $location.path('/project/' + $routeParams.id + '/notes');
-                        });
-                    }
+            $scope.save = function () {
+                if ($scope.form.$valid) {
+                    $scope.projectNote.$save().then(function () {
+                        $location.path('/projects');
+                    });
                 }
-            }]);
+            }
+        }]);
