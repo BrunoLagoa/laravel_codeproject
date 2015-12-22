8f913755d8d990aa49699f5894d625aa385ee591
BrunoLagoa <miroldols@gmail.com>
2015-12-22 20:48:16 -0200

    - LaravelAngular-6.13-Editando project Changes to be committed: 	modified:   .idea/workspace.xml 	modified:   public/build/js/services/project.js 	modified:   resources/assets/js/services/project.js

--- public/build/js/services/project.js
+++ public/build/js/services/project.js
@@ -1,13 +1,16 @@
 angular.module('app.services')
     .service('Project', ['$resource', '$filter', '$httpParamSerializer', 'appConfig',
         function ($resource, $filter, $httpParamSerializer, appConfig) {
+
             function transformData(data) {
                 if (angular.isObject(data) && data.hasOwnProperty('due_date')) {
-                    data.due_date = $filter('date')(data.due_date, 'yyyy-MM-dd');
-                    return $httpParamSerializer(data);
+                    var o = angular.copy(data);
+                    o.due_date = $filter('date')(data.due_date, 'yyyy-MM-dd');
+                    return $httpParamSerializer(o);
                 }
                 return data;
-            };
+            }
+
             return $resource(appConfig.baseUrl + '/project/:id', {id: '@id'}, {
                 save: {
                     method: 'POST',
@@ -18,8 +21,9 @@ angular.module('app.services')
                     transformResponse: function (data, headers) {
                         var o = appConfig.utils.transformResponse(data, headers);
                         if (angular.isObject(o) && o.hasOwnProperty('due_date')) {
-                            var arrayDate = o.due_date.split('-');
-                            o.due_date = new Date(arrayDate[0], arrayDate[1], arrayDate[2]);
+                            var arrayDate = o.due_date.split('-'),
+                                month = parseInt(arrayDate[1])-1;
+                            o.due_date = new Date(arrayDate[0], month, arrayDate[2]);
                         }
                         return o;
                     }
