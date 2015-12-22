30049643f6ddfdeb99533a2f37d924447065cd38
BrunoLagoa <miroldols@gmail.com>
2015-12-22 20:23:24 -0200

    - LaravelAngular-6.12-Transformando due_date Changes to be committed: 	modified:   .idea/workspace.xml 	modified:   public/build/js/services/project.js 	modified:   resources/assets/js/services/project.js

--- public/build/js/services/project.js
+++ public/build/js/services/project.js
@@ -1,16 +1,17 @@
 angular.module('app.services')
     .service('Project', ['$resource', '$filter', '$httpParamSerializer', 'appConfig',
         function ($resource, $filter, $httpParamSerializer, appConfig) {
+            function transformData(data) {
+                if (angular.isObject(data) && data.hasOwnProperty('due_date')) {
+                    data.due_date = $filter('date')(data.due_date, 'yyyy-MM-dd');
+                    return $httpParamSerializer(data);
+                }
+                return data;
+            };
             return $resource(appConfig.baseUrl + '/project/:id', {id: '@id'}, {
                 save: {
                     method: 'POST',
-                    transformRequest: function (data) {
-                        if (angular.isObject(data) && data.hasOwnProperty('due_date')) {
-                            data.due_date = $filter('date')(data.due_date, 'yyyy-MM-dd');
-                            return $httpParamSerializer(data);
-                        }
-                        return data;
-                    }
+                    transformRequest: transformData
                 },
                 get: {
                     method: 'GET',
@@ -18,13 +19,14 @@ angular.module('app.services')
                         var o = appConfig.utils.transformResponse(data, headers);
                         if (angular.isObject(o) && o.hasOwnProperty('due_date')) {
                             var arrayDate = o.due_date.split('-');
-                            o.due_date = new Date(arrayDate[0],arrayDate[1],arrayDate[2]);
+                            o.due_date = new Date(arrayDate[0], arrayDate[1], arrayDate[2]);
                         }
                         return o;
                     }
                 },
                 update: {
-                    method: 'PUT'
+                    method: 'PUT',
+                    transformRequest: transformData
                 }
             });
         }]);
