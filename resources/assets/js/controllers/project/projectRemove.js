5db2d9ffd2c9085f6ad6bc6d4dde4382bb10413d
BrunoLagoa <miroldols@gmail.com>
2015-12-22 22:56:58 -0200

    - LaravelAngular-6.16-Excluindo project Changes to be committed: 	modified:   .idea/workspace.xml 	modified:   public/build/js/controllers/project/projectRemove.js 	modified:   public/build/views/project/remove.html 	modified:   resources/assets/js/controllers/project/projectRemove.js 	modified:   resources/assets/js/views/project/remove.html

--- resources/assets/js/controllers/project/projectRemove.js
+++ resources/assets/js/controllers/project/projectRemove.js
@@ -1,24 +1,16 @@
 angular.module('app.controllers')
-    .controller('ProjectNoteRemoveController',
-        ['$scope', '$location', '$routeParams', 'ProjectNote',
-            function ($scope, $location, $routeParams, ProjectNote) {
-                //$scope.projectNote = ProjectNote.get({id: $routeParams.id});
+    .controller('ProjectRemoveController',
+        ['$scope', '$location', '$routeParams', 'Project',
+            function ($scope, $location, $routeParams, Project) {
 
                 $scope.project_id = $routeParams.id;
-                $scope.projectNote = ProjectNote.get({
-                    id: $routeParams.id,
-                    idNote: $routeParams.idNote
-                });
+                $scope.project = Project.get({id: $routeParams.id});
 
                 //console.log($scope.client);
 
                 $scope.remove = function () {
-                    $scope.projectNote.$delete({
-                        //id: null,
-                        id: $routeParams.id,
-                        idNote: $scope.projectNote.id
-                    }).then(function () {
-                        $location.path('/project/' + $routeParams.id + '/notes');
+                    $scope.project.$delete({id: $scope.project.id}).then(function () {
+                        $location.path('/projects/');
                     });
 
                 }
