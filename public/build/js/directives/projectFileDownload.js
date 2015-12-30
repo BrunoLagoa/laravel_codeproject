55b3673316f362dc835b6ab5d6c426cd73c6b720
BrunoLagoa <miroldols@gmail.com>
2015-12-30 01:10:18 -0200

    - LaravelAngular-8.18-Criando download automatico Changes to be committed: 	modified:   public/build/js/directives/projectFileDownload.js 	modified:   resources/assets/js/directives/projectFileDownload.js

--- public/build/js/directives/projectFileDownload.js
+++ public/build/js/directives/projectFileDownload.js
@@ -1,26 +1,32 @@
 angular.module('app.directives')
     .directive('projectFileDownload',
         ['appConfig', 'ProjectFile', function (appConfig, ProjectFile) {
-        return {
-            restrict: 'E',
-            templateUrl: appConfig.baseUrl + '/build/views/templates/projectFileDownload.html',
-            link: function(scope, element, attr){
+            return {
+                restrict: 'E',
+                templateUrl: appConfig.baseUrl + '/build/views/templates/projectFileDownload.html',
+                link: function (scope, element, attr) {
 
-            },
-            controller: ['$scope', '$element', '$attrs',function($scope, $element, $attrs){
-                $scope.downloadFile = function(){
-                    var anchor = $element.children()[0];
-                    $(anchor).addClass('disabled');
-                    $(anchor).text('Carregando...');
-                    ProjectFile.download({id: null, idFile: $attrs.idFile}, function(data){
-                        $(anchor).removeClass('disabled');
-                        $(anchor).text('Save File');
-                        $(anchor).attr({
-                            href: 'data:application-octet-stream;base64,' + data.file,
-                            download: data.name
-                        })
-                    })
-                }
-            }]
-        };
-    }]);
+                },
+                controller: ['$scope', '$element', '$attrs', '$timeout', function ($scope, $element, $attrs, $timeout) {
+                    $scope.downloadFile = function () {
+                        var anchor = $element.children()[0];
+                        $(anchor).addClass('disabled');
+                        $(anchor).text('Carregando...');
+                        ProjectFile.download({id: null, idFile: $attrs.idFile}, function (data) {
+                            $(anchor).removeClass('disabled');
+                            $(anchor).text('Save File');
+                            $(anchor).attr({
+                                href: 'data:application-octet-stream;base64,' + data.file,
+                                download: data.name
+                            });
+                            $timeout(function () {
+                                $scope.downloadFile = function(){
+                                    
+                                };
+                                $(anchor)[0].click();
+                            });
+                        });
+                    };
+                }]
+            };
+        }]);
