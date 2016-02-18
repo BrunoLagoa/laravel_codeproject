bd4ff95a8e3859ae34c14f4a706f339678bbaa7f
BrunoLagoa <miroldols@gmail.com>
2016-02-18 15:59:25 -0200

    - LaravelAngular-12.24-Removendo problema na diretiva tabProject Changes to be committed: 	modified:   public/build/js/directives/tabProject.js 	modified:   resources/assets/js/directives/tabProject.js

--- resources/assets/js/directives/tabProject.js
+++ resources/assets/js/directives/tabProject.js
@@ -1,18 +1,17 @@
 angular.module('app.directives')
     .directive('tabProject',
-        [function(){
+        [function () {
             return {
                 restrict: 'A',
-                link:function(scope, element, attrs) {
-                    $(element).find('a').click(function(){
+                link: function (scope, element, attrs) {
+                    $(element).find('a').click(function () {
                         var tabContent = $(element).parent().find('.tab-content'),
                             a = $(this);
                         $(element).find('.active').removeClass('active');
                         tabContent.find('.active').removeClass('active');
-                        tabContent.find("[id="+a.attr('aria-controls')+"]").addClass('active');
+                        tabContent.find("[id=" + a.attr('aria-controls') + "]").addClass('active');
                         a.addClass('active');
                     });
                 }
-
             };
         }]);
