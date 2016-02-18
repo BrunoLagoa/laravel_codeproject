6ff1bf1e3b6cb17dae5e4f777e6f15755d2dc594
BrunoLagoa <miroldols@gmail.com>
2016-02-18 01:14:20 -0200

    - LaravelAngular-12.4-Configurando a paginacao Changes to be committed: 	modified:   public/build/js/controllers/project/projectList.js 	modified:   public/build/views/project-file/list.html 	modified:   public/build/views/project-task/list.html 	modified:   public/build/views/project/list.html 	modified:   resources/assets/js/controllers/project/projectList.js 	modified:   resources/assets/js/views/project-file/list.html 	modified:   resources/assets/js/views/project-task/list.html 	modified:   resources/assets/js/views/project/list.html

--- resources/assets/js/controllers/project/projectList.js
+++ resources/assets/js/controllers/project/projectList.js
@@ -1,5 +1,25 @@
 angular.module('app.controllers')
     .controller('ProjectListController', [
         '$scope', '$routeParams', 'Project', function ($scope, $routeParams, Project) {
-            $scope.projects = Project.query();
+
+            $scope.projects = [];
+            $scope.totalProjects = 0;
+            $scope.projecstPerPage = 15;
+
+            $scope.pagination = {
+                current: 1
+            };
+
+            $scope.pageChanged = function (newPage) {
+                getResultsPage(newPage);
+            };
+
+            function getResultsPage(pageNumber) {
+                Project.query({}, function (data) {
+                    $scope.projects = data;
+                    //$scope.totalUsers = result.data.Count
+                });
+            }
+
+            getResultsPage(1);
         }]);
