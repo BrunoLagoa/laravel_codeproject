bb21d6dc993d42ee5cdda773264a0373d52775e5
BrunoLagoa <miroldols@gmail.com>
2016-02-18 11:22:11 -0200

    - LaravelAngular-12.15-Configurando ativacao do item de menu ativo Changes to be committed: 	modified:   public/build/js/app.js 	modified:   public/build/views/templates/menu.html 	modified:   resources/assets/js/app.js 	modified:   resources/assets/js/views/templates/menu.html

--- public/build/js/app.js
+++ public/build/js/app.js
@@ -85,7 +85,8 @@ app.config([
             })
             .when('/home', {
                 templateUrl: 'build/views/home.html',
-                controller: 'HomeController'
+                controller: 'HomeController',
+                title: 'Painel'
             })
 
             // Clients
@@ -113,85 +114,104 @@ app.config([
             // Projects
             .when('/projects', {
                 templateUrl: 'build/views/project/list.html',
-                controller: 'ProjectListController'
+                controller: 'ProjectListController',
+                title: 'Projects'
             })
             .when('/project/new', {
                 templateUrl: 'build/views/project/new.html',
-                controller: 'ProjectNewController'
+                controller: 'ProjectNewController',
+                title: 'Projects'
             })
             .when('/project/:id', {
                 templateUrl: 'build/views/project/show.html',
-                controller: 'ProjectShowController'
+                controller: 'ProjectShowController',
+                title: 'Projects'
             })
             .when('/project/:id/edit', {
                 templateUrl: 'build/views/project/edit.html',
-                controller: 'ProjectEditController'
+                controller: 'ProjectEditController',
+                title: 'Projects'
             })
             .when('/project/:id/remove', {
                 templateUrl: 'build/views/project/remove.html',
-                controller: 'ProjectRemoveController'
+                controller: 'ProjectRemoveController',
+                title: 'Projects'
             })
 
             // Project notes
             .when('/project/:id/notes', {
                 templateUrl: 'build/views/project-note/list.html',
-                controller: 'ProjectNoteListController'
+                controller: 'ProjectNoteListController',
+                title: 'Notas'
             })
             .when('/project/:id/note/:idNote/show', {
                 templateUrl: 'build/views/project-note/show.html',
-                controller: 'ProjectNoteShowController'
+                controller: 'ProjectNoteShowController',
+                title: 'Notas'
             })
             .when('/project/:id/note/new', {
                 templateUrl: 'build/views/project-note/new.html',
-                controller: 'ProjectNoteNewController'
+                controller: 'ProjectNoteNewController',
+                title: 'Notas'
             })
             .when('/project/:id/note/:idNote/edit', {
                 templateUrl: 'build/views/project-note/edit.html',
-                controller: 'ProjectNoteEditController'
+                controller: 'ProjectNoteEditController',
+                title: 'Notas'
             })
             .when('/project/:id/note/:idNote/remove', {
                 templateUrl: 'build/views/project-note/remove.html',
-                controller: 'ProjectNoteRemoveController'
+                controller: 'ProjectNoteRemoveController',
+                title: 'Notas'
             })
 
             // Project File
             .when('/project/:id/files', {
                 templateUrl: 'build/views/project-file/list.html',
-                controller: 'ProjectFileListController'
+                controller: 'ProjectFileListController',
+                title: 'Arquivos'
             })
             .when('/project/:id/file/new', {
                 templateUrl: 'build/views/project-file/new.html',
-                controller: 'ProjectFileNewController'
+                controller: 'ProjectFileNewController',
+                title: 'Arquivos'
             })
             .when('/project/:id/file/:idFile/edit', {
                 templateUrl: 'build/views/project-file/edit.html',
-                controller: 'ProjectFileEditController'
+                controller: 'ProjectFileEditController',
+                title: 'Arquivos'
             })
             .when('/project/:id/file/:idFile/remove', {
                 templateUrl: 'build/views/project-file/remove.html',
-                controller: 'ProjectFileRemoveController'
+                controller: 'ProjectFileRemoveController',
+                title: 'Arquivos'
             })
 
             // Project tasks
             .when('/project/:id/tasks', {
                 templateUrl: 'build/views/project-task/list.html',
-                controller: 'ProjectTaskListController'
+                controller: 'ProjectTaskListController',
+                title: 'Tasks'
             })
             .when('/project/:id/task/:idTask/show', {
                 templateUrl: 'build/views/project-task/show.html',
-                controller: 'ProjectTaskShowController'
+                controller: 'ProjectTaskShowController',
+                title: 'Tasks'
             })
             .when('/project/:id/task/new', {
                 templateUrl: 'build/views/project-task/new.html',
-                controller: 'ProjectTaskNewController'
+                controller: 'ProjectTaskNewController',
+                title: 'Tasks'
             })
             .when('/project/:id/task/:idTask/edit', {
                 templateUrl: 'build/views/project-task/edit.html',
-                controller: 'ProjectTaskEditController'
+                controller: 'ProjectTaskEditController',
+                title: 'Tasks'
             })
             .when('/project/:id/task/:idTask/remove', {
                 templateUrl: 'build/views/project-task/remove.html',
-                controller: 'ProjectTaskRemoveController'
+                controller: 'ProjectTaskRemoveController',
+                title: 'Tasks'
             })
 
             // Project members
@@ -230,6 +250,11 @@ app.run(['$rootScope', '$location', '$http', '$modal', 'httpBuffer', 'OAuth',
             }
         }
     });
+
+    $rootScope.$on('$routeChangeSuccess', function(event, current, previous){
+        $rootScope.pageTitle = current.$$route.title;
+    });
+
     $rootScope.$on('oauth:error', function (event, data) {
         // Ignore `invalid_grant` error - should be catched on `LoginController`.
         if ('invalid_grant' === data.rejection.data.error) {
