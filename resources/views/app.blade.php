a9f45223bee08f1d2363d677bcd55d41ce3eaefc
BrunoLagoa <miroldols@gmail.com>
2016-02-18 01:57:48 -0200

    - LaravelAngular-12.7-Chamando menu com ng-include Changes to be committed: 	modified:   public/build/js/app.js 	new file:   public/build/views/templates/menu.html 	modified:   resources/assets/js/app.js 	new file:   resources/assets/js/views/templates/menu.html 	modified:   resources/views/app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -25,47 +25,8 @@
         <![endif]-->
 </head>
 <body>
-<nav class="navbar navbar-default">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
-                <span class="sr-only">Toggle Navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Laravel</a>
-        </div>
 
-        <div class="collapse navbar-collapse" id="navbar">
-            <ul class="nav navbar-nav">
-                <li><a href="{{ url('/#/login') }}">Welcome</a></li>
-                <li><a href="{{ url('/#/clients') }}">Clients</a></li>
-                <li><a href="{{ url('/#/projects') }}">Projects</a></li>
-            </ul>
-
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="{{ url('/#/logout') }}">Logout</a></li>
-                @if(auth()->guest())
-                    @if(!Request::is('auth/login'))
-                        <li><a href="{{ url('/auth/login') }}">Login</a></li>
-                    @endif
-                    @if(!Request::is('auth/register'))
-                        <li><a href="{{ url('/auth/register') }}">Register</a></li>
-                    @endif
-                @else
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                           aria-expanded="false">{{ auth()->user()->name }} <span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="{{ url('/auth/logout') }}">Logout</a></li>
-                        </ul>
-                    </li>
-                @endif
-            </ul>
-        </div>
-    </div>
-</nav>
+<div ng-include="'build/views/templates/menu.html'"></div>
 
 <div ng-view>
 
