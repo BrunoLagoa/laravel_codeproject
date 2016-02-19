359e46aaf7cb05cb15752313797066469d0e2a78
BrunoLagoa <miroldols@gmail.com>
2016-02-19 19:58:38 -0200

    - LaravelAngular-13.11-Configurando realtime no painel Changes to be committed: 	modified:   public/build/js/controllers/home.js 	modified:   public/build/views/home.html 	modified:   resources/assets/js/controllers/home.js 	modified:   resources/assets/js/views/home.html

--- public/build/js/controllers/home.js
+++ public/build/js/controllers/home.js
@@ -1,4 +1,19 @@
 angular.module('app.controllers')
-    .controller('HomeController', ['$scope', '$cookies', function ($scope, $cookies) {
+    .controller('HomeController', ['$scope', '$cookies', '$timeout', '$pusher', function ($scope, $cookies, $timeout, $pusher) {
         //console.log($cookies.getObject('user').email);
+
+        $scope.tasks = [];
+        var pusher = $pusher(window.client);
+        var channel = pusher.subscribe('user.' + $cookies.getObject('user').id);
+        channel.bind('CodeProject\\Events\\TaskWasIncluded',
+            function (data) {
+                if ($scope.tasks.length == 6) {
+                    $scope.tasks.splice($scope.tasks.length - 1, 1);
+                }
+                $timeout(function () {
+                    $scope.tasks.unshift(data.tasks);
+                }, 300);
+            }
+        );
+
     }]);
